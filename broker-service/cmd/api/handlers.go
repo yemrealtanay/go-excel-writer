80b7a09fd96501cb7bc6f612da6789904138fc9a
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2024-12-10 23:17:54 +0300

    last

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/xuri/excelize/v2"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 type JsonResponse struct {
@@ -19,7 +23,48 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	payload := jsonResponse{
+	excelPath, err := filepath.Abs("ParametreFormu.xlsx")
+	if err != nil {
+		app.errorJson(w, fmt.Errorf("error getting absolute path: %v", err))
+		return
+	}
+	currentDir, err := os.Getwd()
+	if _, err := os.Stat(excelPath); os.IsNotExist(err) {
+		app.errorJson(w, fmt.Errorf("excel file not found at path: %s %v", excelPath, currentDir))
+		return
+	}
+
+	if err != nil {
+		app.errorJson(w, fmt.Errorf("error getting current directory: %v %v %v", err, currentDir, excelPath))
+		return
+	}
+	fmt.Printf("Current working directory: %s\n", currentDir)
+	fmt.Printf("Trying to open file at: %s\n", excelPath)
+
+	var f *excelize.File
+	f, err = excelize.OpenFile(excelPath)
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+	defer f.Close() // Don't forget to close the file
+
+	// Rest of your existing code...
+	sheetName := "Sheet1"
+	rows, _ := f.GetRows(sheetName)
+	newRow := len(rows) + 1
+
+	f.SetCellValue(sheetName, fmt.Sprintf("A%d", newRow), requestData["agirlik"])
+	f.SetCellValue(sheetName, fmt.Sprintf("B%d", newRow), requestData["sogutmaSuresi"])
+	f.SetCellValue(sheetName, fmt.Sprintf("C%d", newRow), requestData["urunAdi"])
+	f.SetCellValue(sheetName, fmt.Sprintf("D%d", newRow), requestData["malAlmaZamani"])
+
+	if err := f.SaveAs(excelPath); err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
+	payload := JsonResponse{
 		Error:   false,
 		Message: "Broker verileri başarıyla aldı",
 		Data:    requestData,
